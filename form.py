69133310737a1f554313a15e01696d62bf3847b3
Ilana Shapiro <ishapiro00@gmail.com>
2024-01-30 22:49:23 -0800

    run some experiments

--- form.py
+++ form.py
@@ -1,6 +1,9 @@
 # Simple MSAF example
 # from __future__ import print_function
-import msaf
+import sys
+sys.path.append('/Users/ilanashapiro/Documents/msaf0_1_80')
+
+import msaf0_1_80
 from midi2audio import FluidSynth
 import os
 
@@ -15,23 +18,27 @@ def convert_dataset_midi_to_wav():
         fs.midi_to_audio(file_path, file_path[:-3] + "wav")
         print("Converted", file_path, "to WAV")
 
-convert_dataset_midi_to_wav()
-
 def segment_audio_MSAF():
   # 1. Select audio file
-  audio_file = "maestro-v3.0.0/2018/MIDI-Unprocessed_Chamber2_MID--AUDIO_09_R3_2018_wav--1.wav"
+  audio_filepath = "LOP_database_06_09_17/liszt_classical_archives/0/bl11_solo.wav"
 
   # 2. Segment the file using the default MSAF parameters (this might take a few seconds)
-  boundaries, labels = msaf.process(audio_file)
+  boundaries, labels = msaf0_1_80.process(audio_filepath, boundaries_id="olda", labels_id="cnmf", hier=True)
+  print(boundaries)
+  print(labels)
   # 3. Save segments using the MIREX format
-  out_file = 'segments.txt'
-  print('Saving output to %s' % out_file)
-  msaf.io.write_mirex(boundaries, labels, out_file)
+  # out_file = audio_filepath[:-4] + '_segments1.txt'
+  # print('Saving output to %s' % out_file)
+  # msaf0_1_80.io.write_mirex(boundaries, labels, out_file)
 
   # 4. Evaluate the results
   # evals = msaf.eval.process(audio_file)
   # print(evals)
 
+# convert_dataset_midi_to_wav()
+segment_audio_MSAF()
 
-
+# print(msaf0_1_80.features_registry)
+# print(msaf0_1_80.get_all_boundary_algorithms())
+# print(msaf0_1_80.get_all_label_algorithms())
 
