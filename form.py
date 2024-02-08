54e2e2111099778034fa591b6072dcffe616ed62
Ilana Shapiro <ishapiro00@gmail.com>
2024-02-08 00:42:16 -0800

    add new libraries

--- form.py
+++ form.py
@@ -2,7 +2,15 @@
 # from __future__ import print_function
 import sys
 sys.path.append('/Users/ilanashapiro/Documents/msaf0_1_80')
+sys.path.append('/Users/ilanashapiro/Library/musicaiz-0.1.2')
+sys.path.append('/Users/ilanashapiro/Library/MIDI-BERT') # https://github.com/wazenmai/MIDI-BERT
+sys.path.append('/Users/ilanashapiro/Library/midi_melody_extraction') # https://github.com/bytedance/midi_melody_extraction
+sys.path.append('/Users/ilanashapiro/Library/motif_discovery') # https://github.com/Tsung-Ping/motif_discovery
+sys.path.append('/Users/ilanashapiro/Library/audio_to_midi_melodia') # https://github.com/justinsalamon/audio_to_midi_melodia
+sys.path.append('/Users/ilanashapiro/Library/functional-harmony') # https://github.com/Tsung-Ping/functional-harmony
+sys.path.append('/Users/ilanashapiro/Library/Harmony-Transformer') # https://github.com/Tsung-Ping/Harmony-Transformer
 
+import py_midicsv as pm
 import msaf
 from midi2audio import FluidSynth
 import os
@@ -18,8 +26,16 @@ def convert_dataset_midi_to_wav():
         fs.midi_to_audio(file_path, file_path[:-3] + "wav")
         print("Converted", file_path, "to WAV")
 
+def midi_to_csv():
+  midi_filepath = "LOP_database_06_09_17/liszt_classical_archives/0/bl11_solo.mid" # for now
+  csv_string = pm.midi_to_csv(midi_filepath)
+
+  with open(midi_filepath[:-3] + "csv", "w") as f:
+    f.writelines(csv_string)
+
+midi_to_csv()
+
 def segment_audio_MSAF():
-  # 1. Select audio file
   audio_filepath = "LOP_database_06_09_17/liszt_classical_archives/0/bl11_solo.wav"
 
   # hierarchical: scluster, olda (boundaries only, use with fmc2d), vmo
@@ -37,7 +53,7 @@ def segment_audio_MSAF():
   # print(evals)
 
 # convert_dataset_midi_to_wav()
-segment_audio_MSAF()
+# segment_audio_MSAF()
 
 # print(msaf0_1_80.features_registry)
 # print(msaf0_1_80.get_all_boundary_algorithms())
